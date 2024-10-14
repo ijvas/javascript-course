c12bda2c370cb54dc8a8335ca65c17d736e7f3c1
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-10-14 08:03:38 -0300

    Arrays, recorrer, metodos y propiedades mas comunes

---
+++ clase6/index.js
@@ -0,0 +1,44 @@
+//                                  Clase 6: Arrays
+
+
+
+
+// const seriesDelFinde = ['Prison Break', 'Los anillos de poder', 'Ted Lasso'];
+// const pelisDelFinde = ['El Hoyo 2', 'Locomia', 'Spiderman']
+
+//Acceder a elementos del Array
+
+// let serieIsma = seriesDelFinde[0]
+// let serieHugo = seriesDelFinde[1]
+// let serieWally = seriesDelFinde[2]
+
+// console.log(serieWally)
+
+
+
+//Recorrer el array:
+
+// for (i=0; i < seriesDelFinde.length; i++){
+
+//     console.log(seriesDelFinde[i])
+
+// }
+
+
+
+
+
+//     Métodos y propiedades más comunes: .length  .toString()  .push()   .join()    .concat()    .slice()
+
+// console.log(seriesDelFinde.length)
+// console.log(seriesDelFinde.toString())
+// seriesDelFinde.push('Agatha')
+// console.log(seriesDelFinde)
+// console.log(seriesDelFinde.join('+'))
+
+// let finde = seriesDelFinde.concat(pelisDelFinde);
+// console.log(finde.length)
+// console.log(finde.slice(0,10))
+
+
+
