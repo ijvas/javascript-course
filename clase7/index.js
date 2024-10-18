0562baebdb64e243ca8817370b828b3765251658
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-10-18 18:11:04 -0300

    sessionStorage.setItem('clave',valor) y sessionStorage.getItem('clave')

--- clase7/index.js
+++ clase7/index.js
@@ -1,9 +1,33 @@
 //                                          localStorage
 
 
-localStorage.setItem('bienvenida', '¡Hola Guacho!');
+// almacenar información
+// localStorage.setItem('bienvenida', '¡Hola Guacho!');
 
 
-let mensaje = localStorage.getItem('bienvenida')
+// //acceder a información
+// let mensaje = localStorage.getItem('bienvenida')
 
-alert(mensaje)
+// alert(mensaje)
+
+
+
+
+
+
+//                                            sessionStorage
+
+
+// almacenar información
+sessionStorage.setItem('seleccionados', [1,2,3]);
+sessionStorage.setItem('esValido', false);
+sessionStorage.setItem('email', 'info@mail.com');
+
+
+
+//acceder a información
+let lista = sessionStorage.getItem('seleccionados').split(',');
+let bandera = (sessionStorage.getItem('esValido') == 'true');
+let email = sessionStorage.getItem('email')
+
+console.log(typeof lista)
