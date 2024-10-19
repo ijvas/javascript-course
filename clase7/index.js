2e7731991ed2abf8c4ae72323e2027957fde8efc
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-10-19 18:32:51 -0300

    Recorrer el localStorage y el sessionStorage

--- clase7/index.js
+++ clase7/index.js
@@ -2,8 +2,11 @@
 
 
 // almacenar información
-// localStorage.setItem('bienvenida', '¡Hola Guacho!');
-
+localStorage.setItem('bienvenida', '¡Hola Guacho!');
+localStorage.setItem('seleccionados', [1,2,3]);
+localStorage.setItem('esValido', false);
+localStorage.setItem('holaQuehace', 'hola')
+localStorage.setItem('UoO', 'es magike')
 
 // //acceder a información
 // let mensaje = localStorage.getItem('bienvenida')
@@ -18,16 +21,26 @@
 //                                            sessionStorage
 
 
-// almacenar información
-sessionStorage.setItem('seleccionados', [1,2,3]);
-sessionStorage.setItem('esValido', false);
-sessionStorage.setItem('email', 'info@mail.com');
+// // almacenar información
+// sessionStorage.setItem('seleccionados', [1,2,3]);
+// sessionStorage.setItem('esValido', false);
+// sessionStorage.setItem('email', 'info@mail.com');
 
 
 
-//acceder a información
-let lista = sessionStorage.getItem('seleccionados').split(',');
-let bandera = (sessionStorage.getItem('esValido') == 'true');
-let email = sessionStorage.getItem('email')
+// //acceder a información
+// let lista = sessionStorage.getItem('seleccionados').split(',');
+// let bandera = (sessionStorage.getItem('esValido') == 'true');
+// let email = sessionStorage.getItem('email')
 
-console.log(typeof lista)
+// console.log(typeof lista)
+
+
+
+
+//                                                 Recorrer el local o session Storage
+
+for (let i = 0; i < localStorage.length; i++){
+    let variable = localStorage.key(i)
+    console.log('Clave: ' + variable)
+}
