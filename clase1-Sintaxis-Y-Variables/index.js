710ea2ab5a1f887a343807924f1b8ca9d0dbd815
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-10-26 11:33:17 -0300

    Le empezamos a dar un orden al proyecto para que el codigo quede dividido por clases dictadas, CLASE 1

---
+++ clase1-Sintaxis-Y-Variables/index.js
@@ -0,0 +1,21 @@
+alert('Ejemplo 1');
+
+let nombre = prompt('Ingrese su nombre: ');
+
+console.log(`Hola, ${nombre}!`);
+
+alert('Ejemplo 2');
+
+numGuardado = 5;
+
+numCliente = parseFloat(prompt('Ingrese un número: '));
+
+suma = numCliente + numGuardado;
+
+console.log(suma);
+
+alert('Ejemplo 3');
+
+let apellido = prompt('Ingrese su apellido: ');
+
+alert(`${nombre} ${apellido}`);
--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// alert('Ejemplo 1');
-
-// let nombre = prompt('Ingrese su nombre: ');
-
-// console.log(`Hola, ${nombre}!`);
-
-// alert('Ejemplo 2');
-
-// numGuardado = 5;
-
-// numCliente = parseFloat(prompt('Ingrese un número: '));
-
-// suma = numCliente + numGuardado;
-
-// console.log(suma);
-
-// alert('Ejemplo 3');
-
-// let apellido = prompt('Ingrese su apellido: ');
-
-// alert(`${nombre} ${apellido}`);
-
-
 // if (!nombre){
 //     console.log('Que capo!');
 // }else{
