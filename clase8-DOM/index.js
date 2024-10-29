2a63c82ff78204e3027497147006f3223eb6cabb
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-10-29 07:11:40 -0300

    document.getElementsByTagName('')

--- clase8-DOM/index.js
+++ clase8-DOM/index.js
@@ -11,6 +11,13 @@ let paises = document.getElementsByClassName('paises')
 console.log(paises[0].innerHTML)
 
 
+let contenedores = document.getElementsByTagName('div')
+
+console.log(contenedores[0].innerHTML)
+console.log(contenedores[1].innerHTML)
+console.log(contenedores[2].innerHTML)
+
+
 
 
 
