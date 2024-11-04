ec726fbab1b45da3d06233f7332881c08fcdeea6
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-04 07:38:54 -0300

    formas de crear eventos de click - addEventListener('evento',function) elemento.onclick = function

---
+++ clase9-Eventos/index.js
@@ -0,0 +1,19 @@
+
+//                                          Forma de crear un evento número 1
+
+let respuestaClick = () => alert('Me clickeaste')
+
+// let boton = document.getElementById('btnFooter')
+// boton.addEventListener('click', respuestaClick)
+
+
+//                                            Forma de crear un evento número 2
+let boton2 = document.getElementById('btnFooter2')
+boton2.onclick = () => {alert('Presionaste el boton 2')}
+
+
+
+
+
+
+
