5bc58114322f41c45d352e72432316bdef0afad6
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-06 07:24:44 -0300

    Combinación de selectores

--- clase11-Jquery_Selectores/index.js
+++ clase11-Jquery_Selectores/index.js
@@ -14,3 +14,18 @@ $('.ulFooter')
 $('li')
 
 
+
+//              Combinación de selectores
+
+//selecciona todos los <li> de la página
+$('li')
+
+//selecciona todos los <a> de la pagina:
+$('a')
+
+//selecciona todos los <p> y los <a>:
+$('p, a')
+
+//selecciona un ul con clase ulFooter, que tenga un hijo <li>:
+$('ul.ulFooter li')
+
