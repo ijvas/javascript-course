9039f82ade69de392a93a26423980b178369a3a6
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-07 06:03:45 -0300

    Selectores avanzados

--- clase11-Jquery_Selectores/index.js
+++ clase11-Jquery_Selectores/index.js
@@ -29,3 +29,29 @@ $('p , a')
 //selecciona un ul con clase ulFooter, que tenga un hijo <li>:
 $('ul.ulFooter li')
 
+
+
+
+
+
+
+
+
+//                               Selectores avanzados
+
+
+$( "p:last" ); //Selecciona el último <p> de la página
+
+$( "li:first-child" ); //Selecciona todos los <li> que son primeros hijos
+
+$( "li:last-child" ); //Selecciona todos los <li> que son últimos hijos
+
+$( "li:only-child" ); //Selecciona todos los <li> que sean hijos únicos
+
+$( "li:nth-child(3)" ); //Selecciona todos los <li> que sean el 3er elemento
+
+$( "tr:nth-child(odd)" ); //Selecciona todos los <tr> que sean impares
+
+$( "tr:nth-child(even)" ); //Selecciona todos los <tr> que sean pares
+
+$( "div:nth-child(3n)" ); //Selecciona cada tercer elemento <div>
