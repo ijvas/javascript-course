ef1efd3619cec911fcabfcb6b366af57e16fe974
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-11 06:35:26 -0300

    Evento load

--- clase12-jQuery_selectores_y_eventos/index.js
+++ clase12-jQuery_selectores_y_eventos/index.js
@@ -2,16 +2,25 @@
 // El método ready, nos asegura que el DOM del usuario ya está listo para manipularse.
 
 
+
 //ready con JS Vanilla
 // window.addEventListener('DOMContentLoaded'
 //     , function () {
 //      console.log('El DOM esta listo');
 //     });
-    
+
+
 
 //ready con jQuery
 // $( document ).ready(function(){  console.log( "El DOM esta listo" ) });
 
+
+
+//ready con JS VANILLA pero con el evento LOAD ('ESTE ESPERA A QUE TODAS LAS IMAGENES Y RECURSOS EXTERNOS ESTEN CORRECTAMENTE CARGADOS ANTES DE EJECUTARSE)
+// window.addEventListener ('load', function() {
+//     console.log( 'Todos los elementos de la ventana están cargados ' );
+//    });
+   
 
 
 
@@ -21,6 +30,14 @@ $(function() { console.log('Forma corta con function')})
 
 
 
-//                              Forma corta del ready, con función flecha( arrow function )
+//                          Forma corta del ready, con función flecha( arrow function )
 
 $(() => console.log('Forma corta con funcion flecha'))
+
+
+
+
+
+
+
+
