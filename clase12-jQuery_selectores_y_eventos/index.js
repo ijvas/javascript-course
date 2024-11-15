4ca0f016896a8007cf460dae19b8d1a64b8ba82a
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-15 08:28:07 -0300

    eventos click en shortcut

--- clase12-jQuery_selectores_y_eventos/index.js
+++ clase12-jQuery_selectores_y_eventos/index.js
@@ -51,25 +51,53 @@
 
 
 
-//                           Ejemplo aplicado: Asociar evento a botón creado
+// //                           Ejemplo aplicado: Asociar evento a botón creado
 
 
 
-const productos = [
-    {id:1, nombre: 'Arroz', precio: 125},
-    {id:2, nombre: 'Fideo', precio: 70},
-    {id:3, nombre: 'Pan', precio: 50},
-    {id:4, nombre: 'Flan', precio: 100}
-]
+// const productos = [
+//     {id:1, nombre: 'Arroz', precio: 125},
+//     {id:2, nombre: 'Fideo', precio: 70},
+//     {id:3, nombre: 'Pan', precio: 50},
+//     {id:4, nombre: 'Flan', precio: 100}
+// ]
 
-for (const producto of productos){
-    $('#app').append(`  <div>
-                           <h4>Nombre: ${producto.nombre}</h4>
-                           <b>$${producto.precio}</b>
-                           <br>
-                           <button id="btn${producto.id}">Comprar</button>
-                        </div>
-                        <br>`)
+// for (const producto of productos){
+//     $('#app').append(`  <div>
+//                            <h4>Nombre: ${producto.nombre}</h4>
+//                            <b>$${producto.precio}</b>
+//                            <br>
+//                            <button id="btn${producto.id}">Comprar</button>
+//                         </div>
+//                         <br>`)
 
-    $(`#btn${producto.id}`).on('click', ()=> {console.log(` Compraste ${producto.nombre}`)} )
-}
+//     $(`#btn${producto.id}`).on('click', ()=> {console.log(` Compraste ${producto.nombre}`)} )
+// }
+
+
+
+
+
+//                                              SHORTCUTS
+
+
+
+//                     CLICK()
+
+
+//Agregamos dos botones con jQuery
+$("body").prepend('<button id="btn1">BUTTON</button>');
+$("body").prepend('<button id="btn2">BUTTON</button>');
+//Asociamos el evento click para btn1
+$("#btn1").click(function () {
+ console.log(this);
+});
+//Evento click para btn2 con Arrow function y parámetro e
+$("#btn2").click((e) => {
+ console.log(e.target);
+});
+
+
+
+
+
