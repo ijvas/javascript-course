c38dbe402832f2514ee99b203a8117cf7ae29e89
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-18 07:17:44 -0300

    animación SHOW()

---
+++ clase13-efectos_y_animaciones_con_jquery/index.js
@@ -0,0 +1,15 @@
+//                                             ANIMACIONES
+
+
+//                     elemento.animación(opcional:velocidad, opcional:callback)
+
+
+
+//                                            SHOW
+
+$("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
+$("body").append('<button style="display: none" >BOTON</button>');
+
+
+$("h3, button").toggle(1000);
+
