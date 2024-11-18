a6bf3e0154692041480b7c9d9df29ca2dcb1ba8f
Ismael Vasconcel <ijvasconcel@gmail.com>
2024-11-18 07:23:56 -0300

    callback en el fade

--- clase13-efectos_y_animaciones_con_jquery/index.js
+++ clase13-efectos_y_animaciones_con_jquery/index.js
@@ -10,8 +10,8 @@
 $("body").prepend('<h3 style="display: none" >¡Hola Coder1!</h3>');
 $("body").append('<button style="display: none" >BOTON</button>');
 
-
 $("h3, button").show(1000);
+
 
 
 
@@ -34,3 +34,16 @@ $("h3, button").show(1000);
 //                                     Velocidad del FADE 
 
 // fadeIn('slow')   fadeIn('fast')      fadeIn(1000)
+
+
+
+
+
+//                                      Callback del fade
+
+
+//Ocultamos con fadeOut() todos los <h3>
+$("h3").fadeOut("slow", function(){
+    //Cuando termina de ocultarse el elemento lo mostramos nuevamente
+    $("h3").fadeIn(1000);
+   }); 
